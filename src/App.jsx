6acbbee7a034343a8178e00fe9c2e73d694247e0
glebkon <kondakovgleb12@gmail.com>
2024-02-28 13:45:04 +0200

    add brackets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import quotes from './assets/quotes'
+import { useState, useEffect } from "react";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import "./App.css";
+import quotes from "./assets/quotes";
 
 const getRandomQuote = () => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -25,7 +26,7 @@ function App() {
   }, [textColor]);
 
   const newQuote = () => {
-    const randomColor = getRandomColor()
+    const randomColor = getRandomColor();
     setQuote(getRandomQuote());
     setTextColor(randomColor);
     document.body.style.backgroundColor = randomColor;
@@ -34,13 +35,23 @@ function App() {
   return (
     <div className="container">
       <div id="quote-box" style={{ color: textColor }}>
-        <p id="text">«{quote.text}»</p>
+        <p id="text">
+          <FaQuoteLeft size="20" style={{ marginRight: "10px" }} />
+          {quote.text}
+          <FaQuoteRight size="20" style={{ marginLeft: "10px" }} />
+        </p>
         <p id="author">— {quote.author}</p>
-        <button className="btn" id="new-quote" style={{ backgroundColor: textColor }} onClick={newQuote}>New Quote</button>
-        {/* <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote.text}" - ${quote.author}`)}`} id="tweet-quote">Tweet</a> */}
+        <button
+          className="btn"
+          id="new-quote"
+          style={{ backgroundColor: textColor }}
+          onClick={newQuote}
+        >
+          New Quote
+        </button>
       </div>
     </div>
   );
-};
+}
 
-export default App
+export default App;
